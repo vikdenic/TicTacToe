dc57d5102621711e219c8185ee0bcac36eec58b2
Naomi Himley <naomihimley@gmail.com>
2014-05-16 14:49:42 -0500

    Cat's Game Alert added, and Play Again button to Reset Game

--- development/TicTacToeFolder/TicTacToe/ViewController.m
+++ development/TicTacToeFolder/TicTacToe/ViewController.m
@@ -8,7 +8,7 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIAlertViewDelegate>
 @property (weak, nonatomic) IBOutlet UILabel *myLabelOne;
 @property (weak, nonatomic) IBOutlet UILabel *myLabelTwo;
 @property (weak, nonatomic) IBOutlet UILabel *myLabelThree;
@@ -20,7 +20,7 @@
 @property (weak, nonatomic) IBOutlet UILabel *myLabelNine;
 @property (weak, nonatomic) IBOutlet UILabel *whichPlayerLabel;
 @property (nonatomic) int testInt;
-@property (assign) BOOL catChecker;
+@property (assign) BOOL catCheckerVar;
 
 @end
 
@@ -31,6 +31,7 @@
 {
     [super viewDidLoad];
     self.testInt = 1;
+    self.catCheckerVar = YES;
 }
 
 
@@ -39,22 +40,25 @@
 //}
 
 //helper method
--(BOOL)changePlayer:(UILabel *)tappedLabel
+-(BOOL)changePlayerAndLabel:(UILabel *)tappedLabel
 {
     self.testInt += 1; //or ++
-    if(self.testInt % 2 == 0){
+    if(self.testInt % 2 == 0 && [tappedLabel.text isEqualToString:@""]){
         tappedLabel.text = @"X";
         tappedLabel.textColor = [UIColor blueColor];
         self.whichPlayerLabel.text = @"Player 1";
-        return NO;
+        return YES;
     }
-    else{
+    else if(self.testInt % 2 != 0 && [tappedLabel.text isEqualToString:@""]){
         tappedLabel.text =@"O";
         self.whichPlayerLabel.text = @"Player 2";
         tappedLabel.textColor = [UIColor redColor];
         return YES;
-    }
+    }else{
+        self.testInt --;
+        return NO;
 
+    }
 }
 
 
@@ -64,31 +68,31 @@
 //    NSLog(@"%@", NSStringFromCGPoint(point) );
 
     if(CGRectContainsPoint(self.myLabelOne.frame, point)){
-        [self changePlayer:self.myLabelOne];
+        [self changePlayerAndLabel:self.myLabelOne];
     }
     if (CGRectContainsPoint(self.myLabelTwo.frame, point)) {
-        [self changePlayer:self.myLabelTwo];
+        [self changePlayerAndLabel:self.myLabelTwo];
     }
     if (CGRectContainsPoint(self.myLabelThree.frame, point)) {
-        [self changePlayer:self.myLabelThree];
+        [self changePlayerAndLabel:self.myLabelThree];
     }
     if (CGRectContainsPoint(self.myLabelFour.frame, point)) {
-        [self changePlayer:self.myLabelFour];
+        [self changePlayerAndLabel:self.myLabelFour];
     }
     if (CGRectContainsPoint(self.myLabelFive.frame, point)) {
-        [self changePlayer:self.myLabelFive];
+        [self changePlayerAndLabel:self.myLabelFive];
     }
     if (CGRectContainsPoint(self.myLabelSix.frame, point)) {
-        [self changePlayer:self.myLabelSix];
+        [self changePlayerAndLabel:self.myLabelSix];
     }
     if (CGRectContainsPoint(self.myLabelSeven.frame, point)) {
-        [self changePlayer:self.myLabelSeven];
+        [self changePlayerAndLabel:self.myLabelSeven];
     }
     if (CGRectContainsPoint(self.myLabelEight.frame, point)) {
-        [self changePlayer:self.myLabelEight];
+        [self changePlayerAndLabel:self.myLabelEight];
     }
     if (CGRectContainsPoint(self.myLabelNine.frame, point)) {
-        [self changePlayer:self.myLabelNine];
+        [self changePlayerAndLabel:self.myLabelNine];
     }
 
     [self whoWon:self.myLabelTwo :self.myLabelFive :self.myLabelEight];
@@ -99,24 +103,14 @@
     [self whoWon:self.myLabelSeven :self.myLabelEight :self.myLabelNine];
     [self whoWon:self.myLabelOne :self.myLabelFive :self.myLabelNine];
     [self whoWon:self.myLabelSeven :self.myLabelFive :self.myLabelThree];
-
-
-
-
-//calling whoWon helper method here
-
-    //test each possible winning combo with each click
-//    if 258
-//    if 369
-//    if 147
-//    if 123
-//    if 456
-//    if 789
-//    if 159
-//    if 753
-//    alert view winner
+    [self checkForCat];
+    NSLog(@"%i", self.testInt);
 
 }
+
+//already tapped method to be called in onLableTapped.
+//whenever a labels text is !@"" you cannot click it.
+
 
 //create an alert that displays one of two variable: player 1 or player 2
 //based on if there is a trio of x's or o's
@@ -127,18 +121,19 @@
 
     if ([testLabel1.text isEqual: @"X" ] && [testLabel2.text isEqual: @"X"] && [testLabel3.text isEqual: @"X" ] )
     {
-        UIAlertView *xWon = [[UIAlertView alloc]initWithTitle:@"We have a winner!" message:@"X Win" delegate:nil cancelButtonTitle:@"ok" otherButtonTitles:nil, nil];
+        UIAlertView *xWon = [[UIAlertView alloc]initWithTitle:@"We have a winner!" message:@"X Win" delegate:self cancelButtonTitle:@"Play Again!" otherButtonTitles:nil, nil];
         [xWon show];
         //CATSGAME NOT TRUE
-//        [self.catChecker = NO];
+        self.catCheckerVar = NO;
+
     }
 
     else if ([testLabel1.text isEqual: @"O" ] && [testLabel2.text isEqual: @"O"] && [testLabel3.text isEqual: @"O" ] )
     {
-        UIAlertView *oWon = [[UIAlertView alloc]initWithTitle:@"We have a winner!" message:@"O Win" delegate:nil cancelButtonTitle:@"ok" otherButtonTitles:nil, nil];
+        UIAlertView *oWon = [[UIAlertView alloc]initWithTitle:@"We have a winner!" message:@"O Win" delegate:self cancelButtonTitle:@"Play Again!" otherButtonTitles:nil, nil];
         [oWon show];
         //CATSGAME NOT TRUE
-//        [self.catChecker = NO];
+        self.catCheckerVar = NO;
     }
 
     return YES;
@@ -146,26 +141,47 @@
 
 //cats game method
 ////checking if each space isn't empty
-//-(BOOL)checkForCat{
-//
-//        if (![self.myLabelOne.text isEqualToString:@""]
-//            && ![self.myLabelTwo.text isEqualToString:@""]
-//            && ![self.myLabelThree.text isEqualToString:@""]
-//            && ![self.myLabelFour.text isEqualToString:@""]
-//            && ![self.myLabelFive.text isEqualToString:@""]
-//            && ![self.myLabelSix.text isEqualToString:@""]
-//            && ![self.myLabelSeven.text isEqualToString:@""]
-//            && ![self.myLabelEight.text isEqualToString:@""]
-//            && ![self.myLabelNine.text isEqualToString:@""]
-//        {
-//
-//
-//            }
-//
-//
-//
-//
-//}
+-(BOOL)checkForCat{
 
+        if (![self.myLabelOne.text isEqualToString:@""]
+            && ![self.myLabelTwo.text isEqualToString:@""]
+            && ![self.myLabelThree.text isEqualToString:@""]
+            && ![self.myLabelFour.text isEqualToString:@""]
+            && ![self.myLabelFive.text isEqualToString:@""]
+            && ![self.myLabelSix.text isEqualToString:@""]
+            && ![self.myLabelSeven.text isEqualToString:@""]
+            && ![self.myLabelEight.text isEqualToString:@""]
+            && ![self.myLabelNine.text isEqualToString:@""]
+            && self.catCheckerVar == YES)
+        {
+            UIAlertView *catsGame = [[UIAlertView alloc]initWithTitle:@"No Winner" message:@"Cat's Game" delegate:self cancelButtonTitle:@"Play Again!" otherButtonTitles:nil, nil];
+            [catsGame show];
+            return YES;
+                  }
+
+        return NO;
+}
+
+//helper method that uses alert view delegate
+//takes in two string parameters one for title one for message
+//1 set the alert.message to whatever string parameter is passed in
+//have the button reset all labels to @""
+
+-(void)alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger)buttonIndex
+{
+    if (buttonIndex == 0) {
+        self.myLabelOne.text = @"";
+        self.myLabelTwo.text = @"";
+        self.myLabelThree.text = @"";
+        self.myLabelFour.text = @"";
+        self.myLabelFive.text = @"";
+        self.myLabelSix.text = @"";
+        self.myLabelSeven.text = @"";
+        self.myLabelEight.text = @"";
+        self.myLabelNine.text = @"";
+        self.whichPlayerLabel.text = @"Player 1 Begin!";
+        self.testInt = 1;
+    }
+}
 
 @end
